3536ca3b37b8e6ba81a479266fa27db7b6736c31
Marcel White <marcel.white@gmail.com>
2015-11-25 16:04:49 -0330

    using getbootstrap template...

---
+++ js/givinwing.js
@@ -0,0 +1,22 @@
+/**
+ * Created by whiteco on 9/26/2015.
+ */
+
+$(document).ready(function() {
+
+    $("#scootpose").hover(function () {
+      /*  $("#secretmessage").dialog({
+            modal: true,
+            buttons: {
+                Ok: function() {
+                    $( "#secretmessage" ).dialog( "close" );
+                }
+            }
+        }) */
+    })
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+});
